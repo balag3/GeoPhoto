79b0228628fbccc9676b7d2bcc333b74c98f4c03
balag3 <carbalage@gmail.com>
2017-05-12 15:54:53 +0200

    Adds spring backend project

---
+++ geophoto-backend/src/main/java/com/geophoto/GeophotoBackendApplication.java
@@ -0,0 +1,12 @@
+package com.geophoto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeophotoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeophotoBackendApplication.class, args);
+	}
+}
