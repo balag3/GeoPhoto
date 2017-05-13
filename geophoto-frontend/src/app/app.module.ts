1dfeb9b0747d922bfc09832c9ff628fa810c36ed
balag3 <carbalage@gmail.com>
2017-05-13 11:20:29 +0200

    Adds working gmap demo

--- geophoto-frontend/src/app/app.module.ts
+++ geophoto-frontend/src/app/app.module.ts
@@ -2,8 +2,24 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import {
+  DataTableModule,
+  SharedModule,
+  DropdownModule,
+  TooltipModule,
+  CalendarModule,
+  PickListModule,
+  GrowlModule,
+  DialogModule,
+  MultiSelectModule,
+  PanelModule,
+  GMapModule
+} from "primeng/primeng";
 
 import { AppComponent } from './app.component';
+import {CheckboxModule} from "primeng/components/checkbox/checkbox";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+
 
 @NgModule({
   declarations: [
@@ -12,7 +28,20 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    DataTableModule,
+    SharedModule,
+    DropdownModule,
+    TooltipModule,
+    CalendarModule,
+    PickListModule,
+    GrowlModule,
+    DialogModule,
+    MultiSelectModule,
+    PanelModule,
+    GMapModule,
+    CheckboxModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
